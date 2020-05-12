ce62f1e4519549154e55c77f54aebd5c3c447cd5
Hrishikesh Mulkutkar <51927760+Hrishikesh-3459@users.noreply.github.com>
2020-05-12 16:48:55 +0530

    Delete prob_283.py

--- prob_283.py
+++
@@ -1,8 +0,0 @@
-def moveZeroes(nums):
-    """
-    Do not return anything, modify nums in-place instead.
-    """
-    for i in nums:
-        if (i == 0):
-            nums.pop(nums.index(i))
-            nums.append(i)
